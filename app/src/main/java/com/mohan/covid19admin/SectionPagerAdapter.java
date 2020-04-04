332f04154adff0c9c6bd59b3b7a4c38922e782dc
mohanjnv1 <mohanjnv1@gmail.com>
2020-04-04 11:37:01 +0530

    home page created

---
+++ app/src/main/java/com/mohan/covid19admin/SectionPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.mohan.covid19admin;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+class SectionPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0:
+                SuspectReport suspectReport = new SuspectReport();
+                return suspectReport;
+            case 1:
+                FoodCrisis foodCrisis = new FoodCrisis();
+                return foodCrisis;
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position){
+            case 0:
+                return "SUSPECT REPORTED";
+            case 1:
+                return "FOOD CRISIS";
+            default:
+                return null;
+        }
+    }
+}
